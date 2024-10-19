ae96e1fdd1ee8d5b99004c37227ff5ea501da9aa
yamhokim <yoonho.luke.kim@gmail.com>
2024-10-19 13:56:07 -0400

    updated activity feed filtering feature and created template for the activity feed component

--- base/views.py
+++ base/views.py
@@ -76,7 +76,7 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
-    room_messages = Message.objects.filter(Q(room__name__icontains=query))
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=query))
 
     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
