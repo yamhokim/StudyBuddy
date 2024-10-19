c5d6919e9a43de36e1550571ec09bc44be980559
yamhokim <yoonho.luke.kim@gmail.com>
2024-10-18 23:19:38 -0400

    Added a check on deleteRoom that only allows host of room to delete room

--- base/views.py
+++ base/views.py
@@ -90,7 +90,12 @@ def updateRoom(request, pk):
 @login_required(login_url="login")
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here!!')
+    
     if request.method == 'POST':
         room.delete()
         return redirect('home')
+    
     return render(request, 'base/delete.html', {'obj': room})
