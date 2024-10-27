3042c4584514a2ca035537fab09602ecbc692f63
yamhokim <yoonho.luke.kim@gmail.com>
2024-10-27 19:58:27 -0400

    completed the mobile version of the activities page

--- base/views.py
+++ base/views.py
@@ -191,3 +191,8 @@ def topicsPage(request):
     topics = Topic.objects.filter(name__icontains=query)
     context = {'topics': topics}
     return render(request, 'base/topics.html', context)
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    context = {'room_messages': room_messages}
+    return render(request, 'base/activity.html', context)
