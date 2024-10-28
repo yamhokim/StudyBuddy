9510852c7a0778961017ea0ce6142a7a91381cb0
yamhokim <yoonho.luke.kim@gmail.com>
2024-10-27 22:41:51 -0400

    updated the user registration page to reflect the new fields in the custom user model

--- base/views.py
+++ base/views.py
@@ -4,9 +4,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message, User
-from .forms import RoomForm, UserForm
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 # Create your views here.
 
@@ -46,10 +45,10 @@ def logoutUser(request):
     return redirect('home')
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             # Generate a model of the data, without saving it
             user = form.save(commit=False)
