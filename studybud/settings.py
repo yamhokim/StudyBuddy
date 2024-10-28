da30484ba5275b0bf609a00fecc5bb6378a65c0b
yamhokim <yoonho.luke.kim@gmail.com>
2024-10-27 22:23:31 -0400

    created an avatar field for the custom user model and created a media url and root in settings

--- studybud/settings.py
+++ studybud/settings.py
@@ -125,10 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # STATIC_ROOT = 
 
