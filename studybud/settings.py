e52350509f091b3449d0e759cab873b84dcef619
yamhokim <yoonho.luke.kim@gmail.com>
2024-10-27 20:38:19 -0400

    added the django rest framework and updated previous api view for getting routes

--- studybud/settings.py
+++ studybud/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     
     'base.apps.BaseConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- studybud/urls.py
+++ studybud/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('base.urls'))
+    path("", include('base.urls')),
+    path('api/', include('base.api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
