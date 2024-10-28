86b3225f6f9d28b6e58515349671710b87f71d8d
yamhokim <yoonho.luke.kim@gmail.com>
2024-10-27 21:17:02 -0400

    set up serializers for the room data and added django-cors-headers package to allow resources to be accessed on other domains

--- studybud/settings.py
+++ studybud/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     
     'base.apps.BaseConfig',
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "studybud.urls"
@@ -131,3 +134,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
